bd87aa07efd00cae985e0e452e1e05891a416b8d
hemant10-1995 <31190062+hemant10-1995@users.noreply.github.com>
2017-08-22 02:18:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,3 @@
-/*// change the text of the main-text div
-var element = document.getElementById('main-text'
-);
-
-element.innerHTML = 'New Value';
-//Mov the image
-var img = document.getElementById('madi');
-var marginLeft= 0;
-function moveRight () {
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft =marginLeft + 'px';
-}
-img.onclick= function(){
-       var interval =setInterval(moveRight,50);
-};*/
-
-
 //counter code
 
 var button = document.getElementById('count');
