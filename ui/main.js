de86bbcd2af43b6e94cf4b392909e89842e03db0
hemant10-1995 <31190062+hemant10-1995@users.noreply.github.com>
2017-08-22 01:04:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,9 @@ var element = document.getElementById('main-text'
 );
 
 element.innerHTML = 'New Value';
+
+//mov the image
+var img =document.getElementById('img');
+img.onclick =function(){
+    img.style.marginLeft='100px';
+};
