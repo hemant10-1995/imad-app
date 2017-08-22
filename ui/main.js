17d4941249f2ae68f26f51fdd3d9b523057955a5
hemant10-1995 <31190062+hemant10-1995@users.noreply.github.com>
2017-08-23 00:10:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,10 +33,9 @@ button.onclick = function() {
       if(request.readyState == XMLHttpRequest.DONE){
           //take some action
           if(request.status==200){
-              var counter=request.responseText;
+              var counter = request.response.Text;
               var span = document.getElementById('count');
               span.innerHTML = counter.toString();
-              
           }
           
       }
@@ -44,7 +43,7 @@ button.onclick = function() {
       
   };
   //render the variable in correct span
-  request.open('GET','http://hk076262.imad.hasura-app.io/counter',true);
+  request.open('GET','http://hk076262.imad.hasura-app.io/submit-name?name=' + name,true);
   request.send(null);
 };
 //submit name
@@ -53,12 +52,33 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     //Make a request to the server and send the name
-    //Capture the list of name and render it as list 
-    var names = ['name1', 'name2', 'name3', 'name4'];
+    //create a request object
+  var request = new XMLHttpRequest();
+  
+  //capture the responce and store it on variable
+  
+  request.onreadystatechange = function() {
+      if(request.readyState == XMLHttpRequest.DONE){
+          //take some action
+          if(request.status==200){
+             //Capture the list of name and render it as list 
+    var names = request.responseText;
+    name = JSON.parse(names);
     var list = '';
     for(var i=0; i<names.length;i++){
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+          }
+          
+      }
+      // not done yet
+      
+  };
+  //render the variable in correct span
+  request.open('GET','http://hk076262.imad.hasura-app.io/submit-name?name=' + name, true);
+  request.send(null);
+   
 };
+
