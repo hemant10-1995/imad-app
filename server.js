83449442af4c4dab8cb793ed2d109b365458842c
hemant10-1995 <31190062+hemant10-1995@users.noreply.github.com>
2017-08-31 23:20:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.post('/login', function(req, res) {
         if(err) {
           res.status(500).send(err.toString());
     }else{
-        if (result.rows.length == 0){
+        if (result.rows.length ===0){
             res.send(403).send('username/password invalid');
         }else{
             //match the password
