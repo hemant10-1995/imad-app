b149ae5c5e3751385e6eacfd48f155c136247869
hemant10-1995 <31190062+hemant10-1995@users.noreply.github.com>
2017-09-01 00:24:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -79,7 +79,6 @@ submit.onclick = function () {
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
   console.log(username);
-  //var name = nameInput.value;
   request.open('POST','http://hk076262.imad.hasura-app.io/login', true);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify({username:username,password:password}));
