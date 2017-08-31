025ba6eb703f50919a8687856bd27bd47df0e456
hemant10-1995 <31190062+hemant10-1995@users.noreply.github.com>
2017-08-31 22:06:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/hash/:input',function(req, res){
 
 app.post('/create-user',function (req, res){
    //username ,password 
-   //{"username":"hemant","password"}
+   //{"username :"hemant","password :" "password"}
    //JSON
    var username = req.body.username;
    var password = req.body.password;
