26d70fc5251f88a14c7ea2954c8703dcf8dba906
hemant10-1995 <31190062+hemant10-1995@users.noreply.github.com>
2017-09-01 00:43:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user: 'hk076262',
@@ -16,6 +17,11 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret:'SomerandomSecretvalue',
+    cookie: { maxage:1000 * 60 * 60 * 24 * 30}
+    
+}));
 
 
 function createTemplate (data) {
@@ -105,12 +111,14 @@ app.post('/login', function(req, res) {
             var salt = dbString.split('$')[2];
             var hashedPassword = hash(password, salt);//creating hash based on the password submitted and the origional salt
             if(hashedPassword == dbString) {
+                
+                //set the session
+                req.session.auth = {userId: result.rows[0].id};
+                //set cokkies with session id
+                //internall map the session id
+                
+                
                 res.send('Credentials correct');
-                
-                
-                //set a session
-                
-            
                 }else{
                     res.send(403).send('username/password invalid');
                 }   
@@ -121,6 +129,12 @@ app.post('/login', function(req, res) {
        
    });
     
+});
+
+app.get('/check-login',function (req ,res){
+   if (req.session && req.session.auth && req.session.auth.userId){
+       res.send('You Logged in:' + req.session.auth.userId.toString());
+   } 
 });
 
 var pool = new Pool(config);
