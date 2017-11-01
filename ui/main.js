67263fdc969b91d11331fde62039695f6a39df2d
hemant10-1995 <31190062+hemant10-1995@users.noreply.github.com>
2017-11-01 11:11:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -66,7 +66,7 @@ submit.onclick = function () {
                console.log('user logged in');
                alert('Logged in sucessfully ');
    }else if (request.status === 403){
-       alert('password is incorrect');
+       alert('username/password is incorrect');
    }else if (request.status ===500){
        alert('Something went wrong on server');
    }
@@ -79,6 +79,7 @@ submit.onclick = function () {
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
   console.log(username);
+  console.log(password);
   request.open('POST','http://hk076262.imad.hasura-app.io/login', true);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify({username:username,password:password}));
